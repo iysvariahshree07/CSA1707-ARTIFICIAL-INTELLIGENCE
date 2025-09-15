3fc01ccb7d9a4b22dec58986269f65081027711b
iysvariahshree07 <giysvariahshree@gmail.com>
2025-09-15 19:55:31 +0530

    Create EXPERIMENT 31.pl

---
+++ EXPERIMENT 31.pl
@@ -0,0 +1,8 @@
+<!-- WordPress Custom HTML Block -->
+<div class="medical-info">
+  <h1 title="Health Portal">Welcome to Health Diagnosis Portal</h1>
+  <p>This block demonstrates basic HTML tags.</p>
+  <a href="https://www.who.int" title="Visit WHO" target="_blank">
+    Visit WHO Website
+  </a>
+</div>
